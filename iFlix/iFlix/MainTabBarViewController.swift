df17e2fa162f26f7c7ea10fff5e24e0f9205db78
parutech <paruvitu@gmail.com>
2023-09-12 19:21:22 -0300

    feat:finish  MainTabBarViewController

--- iFlix/iFlix/MainTabBarViewController.swift
+++ iFlix/iFlix/MainTabBarViewController.swift
@@ -11,15 +11,15 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
         
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: UpcomingViewController())
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
         
-        vc1.tabBarItem.image = UIImage(systemName: "house")
-        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc1.tabBarItem.image = UIImage(systemName: "house.circle")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle.fill")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
                
@@ -28,11 +28,8 @@ class MainTabBarViewController: UITabBarController {
         vc3.title = "Top Search"
         vc4.title = "Downloads"
                
-               
-               
-               tabBar.tintColor = .label
-               
-               setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        tabBar.tintColor = .label
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
