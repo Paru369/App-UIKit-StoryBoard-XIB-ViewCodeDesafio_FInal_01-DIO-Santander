438f169cb7b2e9a199df11de67b1b094260fb0cc
parutech <paruvitu@gmail.com>
2023-09-14 14:04:48 -0300

    feat:  setting HomeViewControler

--- iFlix/iFlix/Controllers/HomeViewController.swift
+++ iFlix/iFlix/Controllers/HomeViewController.swift
@@ -22,6 +22,10 @@ class HomeViewController: UIViewController {
         
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
+        
+        homeFeedTable.tableHeaderView  = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+     //        = headerView
+       //        configureHeroHeaderView()
     }
     
     override func viewDidLayoutSubviews() {
